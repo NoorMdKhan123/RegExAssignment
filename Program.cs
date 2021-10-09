3ad3b9ff112791c63a0abdcd67480f7db45fe7cd
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-09 14:52:00 +0530

    Done with 1st use case in registration form

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RegExAssignment
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            bool result;
+            string userName;
+            UserRegistration userRegistration = new UserRegistration();
+            Console.WriteLine("Enter first name : ");
+            userName = Console.ReadLine();
+
+            result = userRegistration.ValidateName(userName);
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ UserRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegExAssignment
+{
+    class UserRegistration
+
+    {
+        public string regExPattern = "^[A-Z]{1}[a-z]{2}$";
+        public bool ValidateName(string name)
+        {
+            return Regex.IsMatch(name, regExPattern);
+        }
+    }
+}
